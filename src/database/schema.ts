dac467e1603b55cb18cc915c71a0c0be72b55652
tzniceguy <joeshimbi@gmail.com>
2025-03-31 09:56:41 +0300

    feat: creating the user table

---
+++ src/database/schema.ts
@@ -0,0 +1,14 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+
+export const userTable = sqliteTable("users", {
+  created_at: integer("created_at", { mode: "timestamp" }).default(
+    sql`CURRENT_TIMESTAMP`,
+  ),
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  email: text("email").notNull().unique(),
+  phoneNumber: text("phone_number").notNull().unique(),
+  password: text("password").notNull(),
+});
