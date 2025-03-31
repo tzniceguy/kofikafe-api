37ae357e8644c6baac1d150ea9685545ef46e59a
tzniceguy <joeshimbi@gmail.com>
2025-03-31 11:04:54 +0300

    refactor: adding swagger docs to the api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { swagger } from "@elysiajs/swagger";
+import { authRoutes } from "./routes/auth";
+const app = new Elysia()
+  .use(swagger())
+  .use(authRoutes)
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
