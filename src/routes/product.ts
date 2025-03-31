8977d8b3e1dc58ab605d706e3f08d8cbb2009807
tzniceguy <joeshimbi@gmail.com>
2025-03-31 14:12:09 +0300

    feat: api endpoint to fetch individual product by id

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,10 @@
-import Elysia from "elysia";
+import { Elysia, t } from "elysia";
 import { db } from "../database";
 import { productTable } from "../database/schema";
+import { eq } from "drizzle-orm";
 
-export const productRoutes = new Elysia({ prefix: "/products" }).get(
-  "/",
-  async () => {
+export const productRoutes = new Elysia({ prefix: "/products" })
+  .get("/", async () => {
     try {
       const products = await db.select().from(productTable).all();
       return {
@@ -21,5 +21,41 @@ export const productRoutes = new Elysia({ prefix: "/products" }).get(
         status: 500,
       };
     }
-  },
-);
+  })
+  .get(
+    "/:id",
+    async ({ params }) => {
+      try {
+        const product = await db
+          .select()
+          .from(productTable)
+          .where(eq(productTable.id, params.id));
+
+        if (!product.length) {
+          return {
+            success: false,
+            message: "Product not found",
+            status: 404,
+          };
+        }
+        return {
+          success: true,
+          message: "Product fetched successfully",
+          data: product[0],
+          status: 200,
+        };
+      } catch (error) {
+        console.error("failed to fetch product", error);
+        return {
+          success: false,
+          message: "failed to fetch product",
+          status: 500,
+        };
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric(),
+      }),
+    },
+  );
