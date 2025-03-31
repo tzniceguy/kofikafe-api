65512f18019a97f16dcaa5b9b5eaad3fc61b25f7
tzniceguy <joeshimbi@gmail.com>
2025-03-31 09:16:42 +0300

    feat: setting up database connection with sqlite

---
+++ src/database/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DB_FILE_NAME environment variable is not set");
+}
+const sqlite = new Database(process.env.DB_FILE_NAME!);
+
+export const db = drizzle(sqlite);
