64137cac4da6ada7dc4a812c40036088dc3fd659
tzniceguy <joeshimbi@gmail.com>
2025-03-31 10:23:22 +0300

    refactor: changing environment variable

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DB_FILE_NAME) {
   throw new Error("DB_FILE_NAME environment variable is not set");
 }
 const sqlite = new Database(process.env.DB_FILE_NAME!);
