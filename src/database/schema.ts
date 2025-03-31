29e2ab145d13a9c7f0e4574e4785971c4f5ea9a7
tzniceguy <joeshimbi@gmail.com>
2025-03-31 12:07:20 +0300

    feat: product table definition
    a table to store product information

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -12,3 +12,15 @@ export const userTable = sqliteTable("users", {
   phoneNumber: text("phone_number").notNull().unique(),
   password: text("password").notNull(),
 });
+
+export const productTable = sqliteTable("products", {
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`CURRENT_TIMESTAMP`,
+  ),
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  price: integer("price").notNull(),
+  quantity: integer("quantity").notNull(),
+  imageUrl: text("image_url").notNull(),
+});
