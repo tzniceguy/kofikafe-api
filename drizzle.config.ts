b8bd8b86bf861d8535f0e24ae2929cbfff92bbfa
tzniceguy <joeshimbi@gmail.com>
2025-03-31 09:21:51 +0300

    feat: drizzle configuration for migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME!,
+  },
+  verbose: true,
+  strict: true,
+});
