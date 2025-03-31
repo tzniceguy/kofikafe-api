3a4fc44478e2576be7b7b53fe1bd53ccc38c28fa
tzniceguy <joeshimbi@gmail.com>
2025-03-31 19:29:28 +0300

    refactor: adding details to api documentation

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,28 @@ import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { authRoutes } from "./routes/auth";
 import { productRoutes } from "./routes/product";
+import { orderRoute } from "./routes/order";
+
 const app = new Elysia()
-  .use(swagger())
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "KofiiKafee API",
+          version: "1.0.0",
+          description: "api for a coffee shop",
+        },
+      },
+    }),
+  )
+
   .use(authRoutes)
   .use(productRoutes)
-  .get("/", () => "Hello Elysia")
+  .use(orderRoute)
+  .get("/", () => ({
+    messsage: "Welcome to KofiiKafee API",
+    version: "1.0.0",
+  }))
   .listen(3000);
 
 console.log(
